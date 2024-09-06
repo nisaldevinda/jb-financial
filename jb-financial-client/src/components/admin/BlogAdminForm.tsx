eb800eac190c5f658530587b42d4af2ee91fb5cc
nisaldevinda <nisaldevindar@gmail.com>
2024-09-06 08:57:51 +0530

    blog admin

--- jb-financial-client/src/components/admin/BlogAdminForm.tsx
+++ jb-financial-client/src/components/admin/BlogAdminForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button, Label, TextInput, Select, Textarea } from "flowbite-react";
 import axios from "axios";
-import {SERVER_URL} from "../../Constants.tsx"; // Import axios for the image upload functionality
+import { SERVER_URL } from "../../Constants.tsx"; // Import axios for the image upload functionality
 
 interface BlogContent {
   heading: string;
@@ -26,10 +26,10 @@ interface BlogAdminFormProps {
 }
 
 const BlogAdminForm: React.FC<BlogAdminFormProps> = ({
-                                                       blog: initialBlog,
-                                                       onSave,
-                                                       onCancel,
-                                                     }) => {
+  blog: initialBlog,
+  onSave,
+  onCancel,
+}) => {
   const [blog, setBlog] = useState<Blog>(initialBlog);
   const [file, setFile] = useState<File | null>(null); // State for managing file upload
   const [uploadStatus, setUploadStatus] = useState<string>("");
@@ -39,19 +39,19 @@ const BlogAdminForm: React.FC<BlogAdminFormProps> = ({
   }, [initialBlog]);
 
   const handleInputChange = (
-      e: React.ChangeEvent<
-          HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-      >
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
   ) => {
     const { id, value } = e.target;
     setBlog({ ...blog, [id]: value });
   };
 
   const handleSectionChange = (
-      index: number,
-      field: "heading" | "paragraphs",
-      value: string | string[],
-      paragraphIndex?: number
+    index: number,
+    field: "heading" | "paragraphs",
+    value: string | string[],
+    paragraphIndex?: number
   ) => {
     const updatedContent = [...blog.content];
     if (field === "paragraphs" && paragraphIndex !== undefined) {
@@ -88,8 +88,8 @@ const BlogAdminForm: React.FC<BlogAdminFormProps> = ({
   const removeParagraph = (sectionIndex: number, paragraphIndex: number) => {
     const updatedContent = [...blog.content];
     updatedContent[sectionIndex].paragraphs = updatedContent[
-        sectionIndex
-        ].paragraphs.filter((_, i) => i !== paragraphIndex);
+      sectionIndex
+    ].paragraphs.filter((_, i) => i !== paragraphIndex);
     setBlog({ ...blog, content: updatedContent });
   };
 
@@ -134,204 +134,257 @@ const BlogAdminForm: React.FC<BlogAdminFormProps> = ({
   };
 
   return (
-      <form
-          onSubmit={handleSubmit}
-          className="flex w-full flex-col gap-4 md:gap-4 p-4 shadow-md rounded-2xl bg-off-white"
-      >
-        <h4 className="switzer-sb text-base md:text-2xl">Blog Post Editor</h4>
-        <p className="switzer-sb text-sm md:text-xl text-neutral-mid">
-          {blog._id ? "Edit Blog Post" : "Create New Blog Post"}
-        </p>
-        {blog._id && (
-            <div className="flex flex-col md:flex-row gap-4">
-              <div className="flex-grow">
-                <div className="mb-2 block">
-                  <Label htmlFor="_id" value="Blog ID" className="switzer-md" />
-                </div>
-                <TextInput
-                    id="_id"
-                    type="text"
-                    value={blog._id}
-                    readOnly
-                    required
-                    shadow
-                    className="switzer-r"
-                />
-              </div>
-            </div>
-        )}
-        {/* Existing form fields */}
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-full flex-col gap-4 md:gap-4 p-4 shadow-md rounded-2xl bg-off-white"
+    >
+      <h4 className="switzer-sb text-base md:text-2xl">Blog Post Editor</h4>
+      <p className="switzer-sb text-sm md:text-xl text-neutral-mid">
+        {blog._id ? "Edit Blog Post" : "Create New Blog Post"}
+      </p>
+      {blog._id && (
         <div className="flex flex-col md:flex-row gap-4">
           <div className="flex-grow">
             <div className="mb-2 block">
-              <Label htmlFor="category" value="Blog Category" className="switzer-md" />
-            </div>
-            <Select
-                id="category"
-                value={blog.category}
-                onChange={handleInputChange}
-                required
-                className="switzer-r"
-            >
-              <option value="">Select a Category</option>
-              <option>Investing</option>
-              <option>Finance</option>
-              <option>Informational</option>
-              <option>Other</option>
-            </Select>
-          </div>
-          <div className="flex-grow">
-            <div className="mb-2 block">
-              <Label htmlFor="duration" value="Read Time (in minutes)" className="switzer-md" />
+              <Label htmlFor="_id" value="Blog ID" className="switzer-md" />
             </div>
             <TextInput
-                id="duration"
-                type="text"
-                value={blog.duration}
-                onChange={handleInputChange}
-                required
-                shadow
-                className="switzer-r"
+              id="_id"
+              type="text"
+              value={blog._id}
+              readOnly
+              required
+              shadow
+              className="switzer-r"
             />
           </div>
         </div>
-        <div className="mb-2 block">
-          <Label htmlFor="title" value="Blog Title" className="switzer-md" />
+      )}
+      {/* Existing form fields */}
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="flex-grow">
+          <div className="mb-2 block">
+            <Label
+              htmlFor="category"
+              value="Blog Category"
+              className="switzer-md"
+            />
+          </div>
+          <Select
+            id="category"
+            value={blog.category}
+            onChange={handleInputChange}
+            required
+            className="switzer-r"
+          >
+            <option value="">Select a Category</option>
+            <option>Investing</option>
+            <option>Finance</option>
+            <option>Informational</option>
+            <option>Other</option>
+          </Select>
         </div>
-        <TextInput
-            id="title"
+        <div className="flex-grow">
+          <div className="mb-2 block">
+            <Label
+              htmlFor="duration"
+              value="Read Time (in minutes)"
+              className="switzer-md"
+            />
+          </div>
+          <TextInput
+            id="duration"
             type="text"
-            value={blog.title}
+            value={blog.duration}
             onChange={handleInputChange}
             required
             shadow
             className="switzer-r"
-        />
-        <div className="mb-2 block">
-          <Label htmlFor="description" value="Blog Description" className="switzer-md" />
+          />
         </div>
-        <Textarea
-            id="description"
-            value={blog.description}
-            onChange={handleInputChange}
-            required
-            shadow
-            className="switzer-r h-32"
+      </div>
+      <div className="mb-2 block">
+        <Label htmlFor="title" value="Blog Title" className="switzer-md" />
+      </div>
+      <TextInput
+        id="title"
+        type="text"
+        value={blog.title}
+        onChange={handleInputChange}
+        required
+        shadow
+        className="switzer-r"
+      />
+      <div className="mb-2 block">
+        <Label
+          htmlFor="description"
+          value="Blog Description"
+          className="switzer-md"
         />
-        {/* Image upload field */}
-        <div className="mb-2 block">
-          <Label htmlFor="imageUrl" value="Image URL" className="switzer-md" />
-        </div>
-        <TextInput
-            id="imageUrl"
-            type="text"
-            value={blog.imageUrl}
-            onChange={handleInputChange}
-            required
-            shadow
-            className="switzer-r"
+      </div>
+      <Textarea
+        id="description"
+        value={blog.description}
+        onChange={handleInputChange}
+        required
+        shadow
+        className="switzer-r h-32"
+      />
+      {/* Image upload field */}
+      <div className="mb-2 block">
+        <Label htmlFor="imageUrl" value="Image URL" className="switzer-md" />
+      </div>
+      <TextInput
+        id="imageUrl"
+        type="text"
+        value={blog.imageUrl}
+        onChange={handleInputChange}
+        required
+        shadow
+        className="switzer-r"
+      />
+      <div className="mb-2 block">
+        <Label
+          htmlFor="imageUpload"
+          value="Upload Image"
+          className="switzer-md"
         />
-        <div className="mb-2 block">
-          <Label htmlFor="imageUpload" value="Upload Image" className="switzer-md" />
-        </div>
-        <input type="file" onChange={handleFileChange} />
-        <Button type="button" onClick={handleUpload} className="primary-button switzer-md mt-2">
-          Upload Image
-        </Button>
-        {uploadStatus && <p>{uploadStatus}</p>}
-        {/* Rest of the form remains the same */}
-        <div>
-          <h5 className="switzer-sb text-sm md:text-xl text-neutral-mid">Blog Content Sections</h5>
-          {blog.content.map((section, sectionIndex) => (
-              <div key={sectionIndex} className="flex flex-col gap-4 mt-4">
+      </div>
+      <input type="file" onChange={handleFileChange} />
+      <Button
+        type="button"
+        onClick={handleUpload}
+        className="primary-button switzer-md mt-2"
+      >
+        Upload Image
+      </Button>
+      {uploadStatus && <p>{uploadStatus}</p>}
+      {/* Rest of the form remains the same */}
+      <div>
+        <h5 className="switzer-sb text-sm md:text-xl text-neutral-mid">
+          Blog Content Sections
+        </h5>
+        {blog.content.map((section, sectionIndex) => (
+          <div key={sectionIndex} className="flex flex-col gap-4 mt-4">
+            <div className="mb-2 block">
+              <Label
+                htmlFor={`section-${sectionIndex}-heading`}
+                value={`Section ${sectionIndex + 1} Heading`}
+                className="switzer-md"
+              />
+            </div>
+            <TextInput
+              id={`section-${sectionIndex}-heading`}
+              type="text"
+              value={section.heading}
+              onChange={(e) =>
+                handleSectionChange(sectionIndex, "heading", e.target.value)
+              }
+              required
+              shadow
+              className="switzer-r"
+            />
+
+            {/* Image Upload - New Section */}
+            <div className=" block">
+              <Label
+                htmlFor="section-image-url"
+                value="Section Image"
+                className="switzer-md"
+              />
+            </div>
+            <TextInput
+              id="section-image-url"
+              type="text"
+              value="Image URL"
+              onChange={(e) =>
+                handleSectionChange(sectionIndex, "heading", e.target.value)
+              }
+              required
+              shadow
+              className="switzer-r"
+            />
+            <div className="mb-2 block">
+              <Label
+                htmlFor="imageUpload"
+                value="Upload Section Image"
+                className="switzer-md"
+              />
+            </div>
+            <input type="file" onChange={handleFileChange} />
+
+            {section.paragraphs.map((paragraph, paragraphIndex) => (
+              <div key={paragraphIndex}>
                 <div className="mb-2 block">
                   <Label
-                      htmlFor={`section-${sectionIndex}-heading`}
-                      value={`Section ${sectionIndex + 1} Heading`}
-                      className="switzer-md"
+                    htmlFor={`section-${sectionIndex}-paragraph-${paragraphIndex}`}
+                    value={`Section ${sectionIndex + 1} Paragraph ${
+                      paragraphIndex + 1
+                    }`}
+                    className="switzer-md"
                   />
                 </div>
-                <TextInput
-                    id={`section-${sectionIndex}-heading`}
-                    type="text"
-                    value={section.heading}
-                    onChange={(e) =>
-                        handleSectionChange(sectionIndex, "heading", e.target.value)
-                    }
-                    required
-                    shadow
-                    className="switzer-r"
+                <Textarea
+                  id={`section-${sectionIndex}-paragraph-${paragraphIndex}`}
+                  value={paragraph}
+                  onChange={(e) =>
+                    handleSectionChange(
+                      sectionIndex,
+                      "paragraphs",
+                      e.target.value,
+                      paragraphIndex
+                    )
+                  }
+                  required
+                  shadow
+                  className="switzer-r h-32"
                 />
-                {section.paragraphs.map((paragraph, paragraphIndex) => (
-                    <div key={paragraphIndex}>
-                      <div className="mb-2 block">
-                        <Label
-                            htmlFor={`section-${sectionIndex}-paragraph-${paragraphIndex}`}
-                            value={`Section ${sectionIndex + 1} Paragraph ${paragraphIndex + 1}`}
-                            className="switzer-md"
-                        />
-                      </div>
-                      <Textarea
-                          id={`section-${sectionIndex}-paragraph-${paragraphIndex}`}
-                          value={paragraph}
-                          onChange={(e) =>
-                              handleSectionChange(
-                                  sectionIndex,
-                                  "paragraphs",
-                                  e.target.value,
-                                  paragraphIndex
-                              )
-                          }
-                          required
-                          shadow
-                          className="switzer-r h-32"
-                      />
-                      <Button
-                          type="button"
-                          onClick={() => removeParagraph(sectionIndex, paragraphIndex)}
-                          className="secondary-button switzer-md mt-2"
-                      >
-                        Remove Paragraph
-                      </Button>
-                    </div>
-                ))}
                 <Button
-                    type="button"
-                    onClick={() => addParagraph(sectionIndex)}
-                    className="secondary-button switzer-md mt-2"
+                  type="button"
+                  onClick={() => removeParagraph(sectionIndex, paragraphIndex)}
+                  className="secondary-button switzer-md mt-2"
                 >
-                  Add Paragraph
-                </Button>
-                <Button
-                    type="button"
-                    onClick={() => removeSection(sectionIndex)}
-                    className="secondary-button switzer-md mt-2"
-                >
-                  Remove Section
+                  Remove Paragraph
                 </Button>
               </div>
-          ))}
-          <Button
+            ))}
+            <Button
               type="button"
-              onClick={addSection}
+              onClick={() => addParagraph(sectionIndex)}
               className="secondary-button switzer-md mt-2"
-          >
-            Add Section
-          </Button>
-        </div>
-        <div className="flex justify-between mt-4">
-          <Button type="submit" className="primary-button switzer-md">
-            Save Blog
-          </Button>
-          <Button
+            >
+              Add Paragraph
+            </Button>
+            <Button
               type="button"
-              onClick={onCancel}
-              className="secondary-button switzer-md"
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
+              onClick={() => removeSection(sectionIndex)}
+              className="secondary-button switzer-md mt-2"
+            >
+              Remove Section
+            </Button>
+          </div>
+        ))}
+        <Button
+          type="button"
+          onClick={addSection}
+          className="secondary-button switzer-md mt-2"
+        >
+          Add Section
+        </Button>
+      </div>
+      <div className="flex justify-between mt-4">
+        <Button type="submit" className="primary-button switzer-md">
+          Save Blog
+        </Button>
+        <Button
+          type="button"
+          onClick={onCancel}
+          className="secondary-button switzer-md"
+        >
+          Cancel
+        </Button>
+      </div>
+    </form>
   );
 };
 
